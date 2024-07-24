46d5d6f0581f8e9981e652ea02f98969491a5c8e
sachinthadev <sahansachintha.dev@gmail.com>
2024-07-25 02:19:12 +0530

    make AddEdit Notes and Note Card

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProfileInfo from '../Cards/ProfileInfo';
 import { Navigate, useNavigate } from 'react-router-dom';
+import SearchBar from '../SearchBar/SearchBar';
 
 const Navbar = () => {
+  const [searchQuery, setsearchQuery]=useState("");
   const navigate= useNavigate;
   const onLogout = ()=>{
     navigate("/login");
   };
 
+  const handleSearch= ()=>{
+
+  };
+
+  const onClearSearch=()=>{
+    setsearchQuery("");
+  };
+
+
   return (
-   <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
-    <h2 className='text-xl font-medium text-black py-2'>Notes</h2>
+   <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow '>
+    <h2 className='text-xl font-medium text-black py-2 mr-4'>Notes</h2>
+
+    <SearchBar value={searchQuery} 
+    onChange={({target})=>{
+      setsearchQuery(target.value);
+    }}
+    handleSearch={handleSearch}
+    onClearSearch={onClearSearch}
+    />
 
     <ProfileInfo onLogout={onLogout}/>
    </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import NoteCard from '../../components/Cards/NoteCard'
+import { MdAdd } from 'react-icons/md'
+import AddEditNotes from './AddEditNotes'
 
 const Home = () => {
   return (
  <>
  <Navbar/>
+
+ <div className='container mx-auto'>
+  <div className='grid grid-cols-3 gap-4 mt-8'>
+  <NoteCard title="meeting is tommorow get ready" date="22th july 2024" content="meeting is tommorow get ready " 
+  tags="#Meetin"
+  isPinned={true}
+  onEdit={() => {}}
+  onDelete={() => {}}
+  onPinNote={() => {}} />
+  </div>
+ </div>
+
+ 
+ <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10'onClick={()=>{}}><MdAdd className='text-[32px] text-white'></MdAdd></button>
+
+ <AddEditNotes/>
  </>
   )
 }
