d0c10d4ee5836adc8afd69ceeffaa041728281d7
sachinthadev <sahansachintha.dev@gmail.com>
2024-07-25 15:05:33 +0530

    add edit note and add tag developed.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import NoteCard from '../../components/Cards/NoteCard'
-import { MdAdd } from 'react-icons/md'
+import { MdAdd, MdOutlineAlarmAdd } from 'react-icons/md'
 import AddEditNotes from './AddEditNotes'
+import Modal from 'react-modal';
 
 const Home = () => {
+
+  const [openAddEditModal, setOpenAddEditModal] = useState({
+    isShown:false,
+    type:"add",
+    date: null
+  });
   return (
  <>
  <Navbar/>
@@ -21,9 +28,34 @@ const Home = () => {
  </div>
 
  
- <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10'onClick={()=>{}}><MdAdd className='text-[32px] text-white'></MdAdd></button>
+ <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10'onClick={()=>{
+  setOpenAddEditModal({
+    isShown:true, type:"add", date:"null"
+  });
+ }}><MdAdd className='text-[32px] text-white'/></button>
 
- <AddEditNotes/>
+<Modal isOpen={openAddEditModal.isShown} onRequestClose={()=>{}}
+  style={{
+    overlay:{
+      backgroundColor:"rgba(0,0,0,0.2)",
+      display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+    },
+  }}
+  contentLabel=""
+  className="w-[40%] max-h-3/4 bg-white rounded-md max-auto mt-14 p-5 overflow-scroll "
+  >
+
+ <AddEditNotes 
+ type={openAddEditModal.type}
+ noteData={openAddEditModal.data}
+ onClose={()=> {
+  setOpenAddEditModal({isShown:false, type:"add", date:null});
+ }} />
+ </Modal>
+
+
  </>
   )
 }
